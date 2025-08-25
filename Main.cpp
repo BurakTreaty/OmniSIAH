1834b66fce8ad7355d014f05de53785bde217409
BT-L <buraktreaty@gmail.com>
2025-08-25 13:00:01 +0300

    Todo list updated, processScanner tested

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,13 @@ int main()
     // Create an instance of ProcessScanner class
     processScanner scanner;
 
-    sca
+    scanner.printProcesses();
 
     return 0;
 }
+
+
+/*TODO LIST:
+* Update TODO's on processScanner.cpp
+* Using parent process id, track the process history
+*/
